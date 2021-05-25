81c6606cac3c4c450e8798ac97ccb1b73b39b1ee
niki7025 <46451404+niki7025@users.noreply.github.com>
2021-05-25 16:07:04 +0200

    Revert "Update save_model.py"
    This reverts commit 9783afdea83423438f3e40c814e98d9bd5cec724.

--- save_model.py
+++ save_model.py
@@ -1,4 +1,4 @@
-import tensorrt as trt
+import tensorflow as tf
 from absl import app, flags, logging
 from absl.flags import FLAGS
 from core.yolov4 import YOLO, decode, filter_boxes
@@ -10,13 +10,13 @@ flags.DEFINE_string('output', './checkpoints/yolov4-416', 'path to output')
 flags.DEFINE_boolean('tiny', False, 'is yolo-tiny or not')
 flags.DEFINE_integer('input_size', 416, 'define input size of export model')
 flags.DEFINE_float('score_thres', 0.2, 'define score threshold')
-flags.DEFINE_string('framework', 'trt', 'define what framework do you want to convert (tf, trt, tflite)')
+flags.DEFINE_string('framework', 'tf', 'define what framework do you want to convert (tf, trt, tflite)')
 flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
 
-def save_trt():
+def save_tf():
   STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
 
-  input_layer = trt.keras.layers.Input([FLAGS.input_size, FLAGS.input_size, 3])
+  input_layer = tf.keras.layers.Input([FLAGS.input_size, FLAGS.input_size, 3])
   feature_maps = YOLO(input_layer, NUM_CLASS, FLAGS.model, FLAGS.tiny)
   bbox_tensors = []
   prob_tensors = []
@@ -38,20 +38,20 @@ def save_trt():
         output_tensors = decode(fm, FLAGS.input_size // 32, NUM_CLASS, STRIDES, ANCHORS, i, XYSCALE, FLAGS.framework)
       bbox_tensors.append(output_tensors[0])
       prob_tensors.append(output_tensors[1])
-  pred_bbox = trt.concat(bbox_tensors, axis=1)
-  pred_prob = trt.concat(prob_tensors, axis=1)
+  pred_bbox = tf.concat(bbox_tensors, axis=1)
+  pred_prob = tf.concat(prob_tensors, axis=1)
   if FLAGS.framework == 'tflite':
     pred = (pred_bbox, pred_prob)
   else:
     boxes, pred_conf = filter_boxes(pred_bbox, pred_prob, score_threshold=FLAGS.score_thres, input_shape=tf.constant([FLAGS.input_size, FLAGS.input_size]))
-    pred = trt.concat([boxes, pred_conf], axis=-1)
-  model = trt.keras.Model(input_layer, pred)
+    pred = tf.concat([boxes, pred_conf], axis=-1)
+  model = tf.keras.Model(input_layer, pred)
   utils.load_weights(model, FLAGS.weights, FLAGS.model, FLAGS.tiny)
   model.summary()
   model.save(FLAGS.output)
 
 def main(_argv):
-  save_trt()
+  save_tf()
 
 if __name__ == '__main__':
     try:
