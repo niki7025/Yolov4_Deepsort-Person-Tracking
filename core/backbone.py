0fa48349001bd58c71fd14cc5e826f1fd0893d44
niki7025 <46451404+niki7025@users.noreply.github.com>
2021-05-25 16:02:53 +0200

    Revert "Revert "tensorrt""
    This reverts commit 7807c2ae698a5c57d62b8dfa245d39d9d01b19cb.

--- core/backbone.py
+++ core/backbone.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # coding=utf-8
 
-import tensorflow as tf
+import tensorrt as trt
 import core.common as common
 
 def darknet53(input_data):
@@ -48,7 +48,7 @@ def cspdarknet53(input_data):
         input_data = common.residual_block(input_data,  64,  32, 64, activate_type="mish")
     input_data = common.convolutional(input_data, (1, 1, 64, 64), activate_type="mish")
 
-    input_data = tf.concat([input_data, route], axis=-1)
+    input_data = trt.concat([input_data, route], axis=-1)
     input_data = common.convolutional(input_data, (1, 1, 128, 64), activate_type="mish")
     input_data = common.convolutional(input_data, (3, 3, 64, 128), downsample=True, activate_type="mish")
     route = input_data
@@ -57,7 +57,7 @@ def cspdarknet53(input_data):
     for i in range(2):
         input_data = common.residual_block(input_data, 64,  64, 64, activate_type="mish")
     input_data = common.convolutional(input_data, (1, 1, 64, 64), activate_type="mish")
-    input_data = tf.concat([input_data, route], axis=-1)
+    input_data = trt.concat([input_data, route], axis=-1)
 
     input_data = common.convolutional(input_data, (1, 1, 128, 128), activate_type="mish")
     input_data = common.convolutional(input_data, (3, 3, 128, 256), downsample=True, activate_type="mish")
@@ -67,7 +67,7 @@ def cspdarknet53(input_data):
     for i in range(8):
         input_data = common.residual_block(input_data, 128, 128, 128, activate_type="mish")
     input_data = common.convolutional(input_data, (1, 1, 128, 128), activate_type="mish")
-    input_data = tf.concat([input_data, route], axis=-1)
+    input_data = trt.concat([input_data, route], axis=-1)
 
     input_data = common.convolutional(input_data, (1, 1, 256, 256), activate_type="mish")
     route_1 = input_data
@@ -78,7 +78,7 @@ def cspdarknet53(input_data):
     for i in range(8):
         input_data = common.residual_block(input_data, 256, 256, 256, activate_type="mish")
     input_data = common.convolutional(input_data, (1, 1, 256, 256), activate_type="mish")
-    input_data = tf.concat([input_data, route], axis=-1)
+    input_data = trt.concat([input_data, route], axis=-1)
 
     input_data = common.convolutional(input_data, (1, 1, 512, 512), activate_type="mish")
     route_2 = input_data
@@ -89,15 +89,15 @@ def cspdarknet53(input_data):
     for i in range(4):
         input_data = common.residual_block(input_data, 512, 512, 512, activate_type="mish")
     input_data = common.convolutional(input_data, (1, 1, 512, 512), activate_type="mish")
-    input_data = tf.concat([input_data, route], axis=-1)
+    input_data = trt.concat([input_data, route], axis=-1)
 
     input_data = common.convolutional(input_data, (1, 1, 1024, 1024), activate_type="mish")
     input_data = common.convolutional(input_data, (1, 1, 1024, 512))
     input_data = common.convolutional(input_data, (3, 3, 512, 1024))
     input_data = common.convolutional(input_data, (1, 1, 1024, 512))
 
-    input_data = tf.concat([tf.nn.max_pool(input_data, ksize=13, padding='SAME', strides=1), tf.nn.max_pool(input_data, ksize=9, padding='SAME', strides=1)
-                            , tf.nn.max_pool(input_data, ksize=5, padding='SAME', strides=1), input_data], axis=-1)
+    input_data = trt.concat([trt.nn.max_pool(input_data, ksize=13, padding='SAME', strides=1), trt.nn.max_pool(input_data, ksize=9, padding='SAME', strides=1)
+                            , trt.nn.max_pool(input_data, ksize=5, padding='SAME', strides=1), input_data], axis=-1)
     input_data = common.convolutional(input_data, (1, 1, 2048, 512))
     input_data = common.convolutional(input_data, (3, 3, 512, 1024))
     input_data = common.convolutional(input_data, (1, 1, 1024, 512))
@@ -114,10 +114,10 @@ def cspdarknet53_tiny(input_data):
     input_data = common.convolutional(input_data, (3, 3, 32, 32))
     route_1 = input_data
     input_data = common.convolutional(input_data, (3, 3, 32, 32))
-    input_data = tf.concat([input_data, route_1], axis=-1)
+    input_data = trt.concat([input_data, route_1], axis=-1)
     input_data = common.convolutional(input_data, (1, 1, 32, 64))
-    input_data = tf.concat([route, input_data], axis=-1)
-    input_data = tf.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
+    input_data = trt.concat([route, input_data], axis=-1)
+    input_data = trt.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
 
     input_data = common.convolutional(input_data, (3, 3, 64, 128))
     route = input_data
@@ -125,10 +125,10 @@ def cspdarknet53_tiny(input_data):
     input_data = common.convolutional(input_data, (3, 3, 64, 64))
     route_1 = input_data
     input_data = common.convolutional(input_data, (3, 3, 64, 64))
-    input_data = tf.concat([input_data, route_1], axis=-1)
+    input_data = trt.concat([input_data, route_1], axis=-1)
     input_data = common.convolutional(input_data, (1, 1, 64, 128))
-    input_data = tf.concat([route, input_data], axis=-1)
-    input_data = tf.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
+    input_data = trt.concat([route, input_data], axis=-1)
+    input_data = trt.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
 
     input_data = common.convolutional(input_data, (3, 3, 128, 256))
     route = input_data
@@ -136,11 +136,11 @@ def cspdarknet53_tiny(input_data):
     input_data = common.convolutional(input_data, (3, 3, 128, 128))
     route_1 = input_data
     input_data = common.convolutional(input_data, (3, 3, 128, 128))
-    input_data = tf.concat([input_data, route_1], axis=-1)
+    input_data = trt.concat([input_data, route_1], axis=-1)
     input_data = common.convolutional(input_data, (1, 1, 128, 256))
     route_1 = input_data
-    input_data = tf.concat([route, input_data], axis=-1)
-    input_data = tf.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
+    input_data = trt.concat([route, input_data], axis=-1)
+    input_data = trt.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
 
     input_data = common.convolutional(input_data, (3, 3, 512, 512))
 
@@ -148,18 +148,18 @@ def cspdarknet53_tiny(input_data):
 
 def darknet53_tiny(input_data):
     input_data = common.convolutional(input_data, (3, 3, 3, 16))
-    input_data = tf.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
+    input_data = trt.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
     input_data = common.convolutional(input_data, (3, 3, 16, 32))
-    input_data = tf.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
+    input_data = trt.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
     input_data = common.convolutional(input_data, (3, 3, 32, 64))
-    input_data = tf.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
+    input_data = trt.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
     input_data = common.convolutional(input_data, (3, 3, 64, 128))
-    input_data = tf.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
+    input_data = trt.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
     input_data = common.convolutional(input_data, (3, 3, 128, 256))
     route_1 = input_data
-    input_data = tf.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
+    input_data = trt.keras.layers.MaxPool2D(2, 2, 'same')(input_data)
     input_data = common.convolutional(input_data, (3, 3, 256, 512))
-    input_data = tf.keras.layers.MaxPool2D(2, 1, 'same')(input_data)
+    input_data = trt.keras.layers.MaxPool2D(2, 1, 'same')(input_data)
     input_data = common.convolutional(input_data, (3, 3, 512, 1024))
 
     return route_1, input_data
