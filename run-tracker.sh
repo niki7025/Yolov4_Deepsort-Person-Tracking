16b0272052ddd785861968117051d0fbc5bef1eb
niki7025 <46451404+niki7025@users.noreply.github.com>
2021-05-27 16:20:38 +0200

    Update run-tracker.sh

--- run-tracker.sh
+++ run-tracker.sh
@@ -1,10 +1,10 @@
 CONTAINER="as-15"
-export OPENBLAS_CORETYPE=ARMV8
-sudo docker run $CONTAINER
+
+sudo docker run -it $CONTAINER
 
 printf "CONTAINER IS RUNNING"
 
-python3 object_tracker.py --framework tflite --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info --is_output_pictures --weights ./checkpoints/yolov4-416-fp32.tflite --tiny
+OPENBLAS_CORETYPE=ARMV8 python3 object_tracker.py --framework tflite --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info --is_output_pictures --weights ./checkpoints/yolov4-416-fp32.tflite --tiny
 
 # Copy the frames from container to the host volume.
 # docker cp $CID:/file/path/within/container /host/path/target
