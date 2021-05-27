5fbdc7968db39636f8cb6a4503c2e0ba57b277d0
Atanasov-A <thestubborn@abv.bg>
2021-05-27 10:56:12 +0200

    Cleaned docker file, changed the running script

--- run-tracker.sh
+++ run-tracker.sh
@@ -2,4 +2,6 @@ CONTAINER="as-15"
 
 sudo docker run $CONTAINER
 printf "CONTAINER IS RUNNING"
+OPENBLAS_CORETYPE=ARMV8 python3 object_tracker.py --framework tflite --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info
+
 python3 object_tracker.py --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info
