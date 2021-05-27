84b8e2a8f069663b3aa4e2cfc81b7e21e9632f66
Atanasov-A <thestubborn@abv.bg>
2021-05-27 12:05:38 +0200

    Added command to save container ID, added copy command

--- run-tracker.sh
+++ run-tracker.sh
@@ -1,5 +1,12 @@
 CONTAINER="as-15"
 
-sudo docker run $CONTAINER
-printf "CONTAINER IS RUNNING"
+#sudo docker run $CONTAINER
+
+CID=$(sudo docker run -d $CONTAINER)
+
+printf "CONTAINER IS RUNNING " + $CID
+
 OPENBLAS_CORETYPE=ARMV8 python3 object_tracker.py --framework tflite --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info
+
+# Copy the frames from container to the host volume.
+#docker cp $CID:/file/path/within/container /host/path/target
