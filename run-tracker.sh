59341a2d49833af8f91f2522a3e9db991c822c42
niki7025 <46451404+niki7025@users.noreply.github.com>
2021-05-27 16:32:40 +0200

    Update run-tracker.sh

--- run-tracker.sh
+++ run-tracker.sh
@@ -1,6 +1,6 @@
 CONTAINER="as-15"
 printf "CONTAINER IS RUNNING"
-sudo docker run -it "OPENBLAS_CORETYPE=ARMV8 python3 object_tracker.py --framework tflite --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info --is_output_pictures --weights ./checkpoints/yolov4-416-fp32.tflite --tiny" $CONTAINER
+sudo docker run -it $CONTAINER && OPENBLAS_CORETYPE=ARMV8 python3 object_tracker.py --framework tflite --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info --is_output_pictures --weights ./checkpoints/yolov4-416-fp32.tflite --tiny
 
 
 
