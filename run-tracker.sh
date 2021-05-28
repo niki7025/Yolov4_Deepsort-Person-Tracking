16c0a817a8a58d1056ac3308ea1a4de5ede1fe73
Atanasov-A <thestubborn@abv.bg>
2021-05-28 17:09:25 +0200

    Removed weights flag

--- run-tracker.sh
+++ run-tracker.sh
@@ -1,7 +1,7 @@
 
 printf "CONTAINER IS RUNNING"
 #OPENBLAS_CORETYPE=ARMV8 python3 object_tracker.py --framework tflite --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info --is_output_pictures --weights ./checkpoints/yolov4-416-fp32.tflite --tiny
-OPENBLAS_CORETYPE=ARMV8 python3 object_tracker.py --framework tflite --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info --is_output_pictures --weights ./checkpoints/yolov4-416/
+OPENBLAS_CORETYPE=ARMV8 python3 object_tracker.py --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info --is_output_pictures
 
 # Copy the frames from container to the host volume.
 # docker cp $CID:/file/path/within/container /host/path/target
