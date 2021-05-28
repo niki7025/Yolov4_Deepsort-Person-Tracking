2d38b62e08564d043bac536032134fbe69a4b59a
Atanasov-A <thestubborn@abv.bg>
2021-05-28 14:12:53 +0200

    Optimizations added

--- object_tracker.py
+++ object_tracker.py
@@ -4,10 +4,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import time
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
-print('PHysical devices ', physical_devices)
 if len(physical_devices) > 0:
-    print('devices in if ', physical_devices)
-    #tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 from absl import app, flags, logging
 from absl.flags import FLAGS
 import core.utils as utils
