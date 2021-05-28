5ac518231a9cdaaef2cbf97646021a01932fafac
Atanasov-A <thestubborn@abv.bg>
2021-05-28 16:48:35 +0200

    Added tensorflow weights

--- run-tracker.sh
+++ run-tracker.sh
@@ -1,6 +1,7 @@
 
 printf "CONTAINER IS RUNNING"
-OPENBLAS_CORETYPE=ARMV8 python3 object_tracker.py --framework tflite --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info --is_output_pictures --weights ./checkpoints/yolov4-416-fp32.tflite --tiny
+#OPENBLAS_CORETYPE=ARMV8 python3 object_tracker.py --framework tflite --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info --is_output_pictures --weights ./checkpoints/yolov4-416-fp32.tflite --tiny
+OPENBLAS_CORETYPE=ARMV8 python3 object_tracker.py --framework tflite --pictures_path ./data/pictures/ --output ./outputs/tracker.avi --model yolov4 --dont_show --info --is_output_pictures --weights ./checkpoints/yolov4-416/saved_model.pb --tiny
 
 # Copy the frames from container to the host volume.
 # docker cp $CID:/file/path/within/container /host/path/target
